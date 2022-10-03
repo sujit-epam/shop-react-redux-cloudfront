7b88d0c42ca01a63c25080089c00938fd9d80f8d
Singh, Sujit <Sujit.Singh@McKesson.com>
2022-10-03 10:47:34 +0530

    Added minor visible changes

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,20 @@ import react from "@vitejs/plugin-react";
 import * as path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig( {
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "src"),
+      "~": path.resolve( __dirname, "src" ),
     },
   },
-  plugins: [react()],
+  server: {
+    open: true,
+    port: 3003
+  },
+  plugins: [ react() ],
   test: {
     globals: true,
     environment: "jsdom",
     setupFiles: "./src/setupTests.ts",
   },
-});
+} );
